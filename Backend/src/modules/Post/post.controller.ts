3d7b10d4e1b055148d225b1826a2ad5419aa6731
TAMJID AHMED <ahmed15-4895@diu.edu.bd>
2026-01-04 18:37:55 +0600

    feat(allPost):Search funtionality

--- Backend/src/modules/Post/post.controller.ts
+++ Backend/src/modules/Post/post.controller.ts
@@ -1,44 +1,63 @@
 import { Request, Response } from "express";
 import { postService } from "./post.services";
+import { PostStatus } from "../../../generated/prisma/enums";
 
-const createPost=async(req:Request,res:Response)=>{
-   try {
-    const user=req.user;
+const createPost = async (req: Request, res: Response) => {
+  try {
+    const user = req.user;
     console.log(user);
-    if(!user){
-       return res.status(400).json({
-        message:"Unauthorized",
-       
-    })  
+    if (!user) {
+      return res.status(400).json({
+        message: "Unauthorized",
+      });
     }
-    const result=await postService.createPost(req.body,user.id as string)
+    const result = await postService.createPost(req.body, user.id as string);
     res.status(201).json({
-        result
-    })
-   } catch (error:any) {
-     res.status(400).json({
-        message:"Post creation failed",
-        details:error
-    })
-   }
-}
-const getAllPosts=async(req:Request,res:Response)=>{
-   try {
-    const result=await postService.getAllPosts()
-    res.status(201).json({
-        success:true,
-        message:"Posts Retrived Successfully",
-        data:result
-    })
-   } catch (error:any) {
-     res.status(400).json({
-        success:false,
-        message:"Posts Retrieval failed",
-        details:error
-    })
-   }
-}
+      result,
+    });
+  } catch (error: any) {
+    res.status(400).json({
+      message: "Post creation failed",
+      details: error,
+    });
+  }
+};
 
-export const postController={
-    createPost,getAllPosts
-}
+const getAllPosts = async (req: Request, res: Response) => {
+  try {
+    const { search } = req.query;
+    const searchString = typeof search === "string" ? search : undefined;
+    const tags = req.query.tags ? (req.query.tags as string).split(",") : [];
+
+    const isFeatured = req.query.isFeatured
+      ? req.query.isFeatured === "true"
+        ? true
+        : req.query.isFeatured === "false"
+        ? false
+        : undefined
+      : undefined;
+    const status=req.query.status as PostStatus | undefined
+    const authorId=req.query.authorId as string 
+    const result = await postService.getAllPosts({
+      search: searchString,
+      tags,
+      isFeatured,status, authorId
+    });
+    res.status(201).json({
+      success: true,
+      message: "Posts Retrived Successfully",
+      data: result,
+    });
+  } catch (error: any) {
+    res.status(400).json({
+      success: false,
+      message: "Posts Retrieval failed",
+      details: error,
+    });
+  }
+};
+
+export const postController = {
+  createPost,
+  getAllPosts,
+};
--- Backend/src/modules/Post/post.services.ts
+++ Backend/src/modules/Post/post.services.ts
@@ -1,24 +1,90 @@
-import { Post } from "../../../generated/prisma/client";
+import { Post, PostStatus } from "../../../generated/prisma/client";
+import { PostWhereInput } from "../../../generated/prisma/models";
 import { prisma } from "../../lib/prisma";
 
+const createPost = async (
+  data: Omit<Post, "id" | "createdAt" | "updatedAt" | "authorId">,
+  userid: string
+) => {
+  const result = await prisma.post.create({
+    data: {
+      ...data,
+      authorId: userid,
+    },
+  });
+  return result;
+};
 
-const createPost=async(data: Omit<Post,"id"|"createdAt"|"updatedAt" | "authorId">,userid:string)=>{
-const result =await prisma.post.create({
-  data:{
-    ...data,
-    authorId:userid
+const getAllPosts = async ({
+  search,
+  tags,isFeatured,status, authorId
+}: {
+  search: string | undefined;
+  tags: string[];
+  isFeatured:boolean | undefined,
+  status:PostStatus | undefined,
+   authorId:string 
+}) => {
+
+  
+  const andcondition: PostWhereInput[] = [];
+  if (search) {
+    andcondition.push({
+      OR: [
+        {
+          title: {
+            contains: search as string,
+            mode: "insensitive",
+          },
+        },
+        {
+          content: {
+            contains: search as string,
+            mode: "insensitive",
+          },
+        },
+        {
+          tags: {
+            has: search as string,
+          },
+        },
+      ],
+    });
   }
-})
-return result;
-}
 
-const getAllPosts=async()=>{
-    const result=await prisma.post.findMany()
-    return result
-}
+  if (tags.length > 0) {
+    andcondition.push({
+      tags: {
+        hasEvery: tags,
+      },
+    });
+  }
+  //isFeatured
+  if(typeof isFeatured==="boolean"){
+    andcondition.push({
+      isFeatured
+    })
+  }
+  //status
+  if(status){
+    andcondition.push({
+      status
+    })
+  }
+  if(authorId){
+    andcondition.push({
+      authorId
+    })
+  }
+  const result = await prisma.post.findMany({
+    where: {
+      AND: andcondition,
+    },
+  });
+  return result;
+};
 
-export const postService={
-    createPost,getAllPosts
-}
-
-
+export const postService = {
+  createPost,
+  getAllPosts,
+};
